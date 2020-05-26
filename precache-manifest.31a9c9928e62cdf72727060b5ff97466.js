97909d7e992d3bee6e5139c87f2fb67dafee2b07
Abhay Sharma <abhays948@gmail.com>
2020-05-27 00:27:28 +0530

    Updates

---
+++ precache-manifest.31a9c9928e62cdf72727060b5ff97466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44417a0a294533705a94092b78b3dcf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74beccaebb1f8b8c323f",
+    "url": "/static/css/main.cc365d64.chunk.css"
+  },
+  {
+    "revision": "abb134287ee11eae3705",
+    "url": "/static/js/2.e2c6a038.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.e2c6a038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74beccaebb1f8b8c323f",
+    "url": "/static/js/main.20a46535.chunk.js"
+  },
+  {
+    "revision": "d9f0bb7198f3ce28c33d",
+    "url": "/static/js/runtime-main.0c401d77.js"
+  }
+]);
